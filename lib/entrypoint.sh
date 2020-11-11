4c334095f4c591bcfe0dec50cb90fa9d5e7f60a6
Matt Strayer <matt.strayer@jebbit.com>
2020-11-11 15:01:54 -0500

    add rubocop-shopify, remove rubocop-rspec

--- lib/entrypoint.sh
+++ lib/entrypoint.sh
@@ -2,9 +2,10 @@
 
 set -e
 
-gem install rubocop -v 0.80.1
-gem install rubocop-rspec -v 1.38.1
+gem install rubocop -v 0.93.1
+# gem install rubocop-rspec -v 1.38.1
 gem install rubocop-performance -v 1.5.2
 gem install rubocop-rails -v 2.4.2
+gem install rubocop-shopify -v 1.0.6
 
 ruby /action/lib/index.rb
